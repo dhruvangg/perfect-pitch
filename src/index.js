4dcf421afd4ed58d0fbf0db99b35bac1a44264b8
Dhruvang <dhruvangg@gmail.com>
2025-01-29 20:17:29 +0530

    Updated PORT

--- src/index.js
+++ src/index.js
@@ -55,4 +55,4 @@ app.post('/review', upload.single('resume'), async (req, res) => {
     // })
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT}`));
